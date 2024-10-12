ddf6571f0460f8d51794e2875b362ffbe7bc0c60
Vitor <vitorhiraide@gmail.com>
2024-10-12 19:02:09 -0300

    Projeto CRUD completo feito em sala de aula

---
+++ aula-05-SistemaLoja-CRUD-Completo/controllers/ProdutosController.js
@@ -0,0 +1,71 @@
+import express from "express";
+const router = express.Router();
+
+// ROTA PRODUTOS
+router.get("/produtos", function (req, res) {
+  Produto.findAll().then((produtos) => {
+    res.render("produtos", {
+      produtos: produtos,
+    });
+  });
+});
+
+router.post("/produtos/new", (req, res) => {
+  const nome = req.body.nome;
+  const valor = req.body.valor;
+  const categoria = req.body.categoria;
+  Produto.create({
+    nome: nome,
+    valor: valor,
+    categoria: categoria,
+  }).then(() => {
+    res.redirect("/produtos");
+  });
+});
+
+router.get("/produtos/delete/:id", (req, res) => {
+  const id = req.params.id;
+  Produto.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then(() => {
+      res.redirect("/produtos");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.get("/produtos/edit/:id", (req, res) => {
+  const id = req.params.id;
+  Produto.findByPk(id).then((produto) => {
+    res
+      .render("/produtoEdit", {
+        produto: produto,
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+});
+
+router.post("/produtos/update", (req, res) => {
+  const id = req.body.id;
+  const nome = req.body.nome;
+  const valor = req.body.valor;
+  const categoria = req.body.categoria;
+  Produto.update({
+    nome: nome,
+    valor: valor,
+    categoria: categoria,
+  })
+    .then(() => {
+      res.redirect("/produtos");
+    })
+    .cactch((error) => {
+      console.log(error);
+    });
+});
+export default router;
